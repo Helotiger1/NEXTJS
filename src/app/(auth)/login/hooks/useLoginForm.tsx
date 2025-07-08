77cb5eb8c792d1059f17ee6012b168e3dccd50a5
amijoaliandev <133158542+amijoaliandev@users.noreply.github.com>
2025-07-08 14:36:05 -0400

    Lógica de login: manejo de formulario y navegación
    Se ajustó useLoginForm para controlar el estado del input y redirigir según el tipo de usuario.

--- src/app/(auth)/login/hooks/useLoginForm.tsx
+++ src/app/(auth)/login/hooks/useLoginForm.tsx
@@ -2,34 +2,37 @@ import { useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 export function useLoginForm() {
-    const router = useRouter();
-    const searchParams = useSearchParams();
-    const tipo = searchParams.get("tipo"); // "cliente" o "empleado"
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const tipo = searchParams.get("tipo"); // "clientee" o "empleado"
 
-    const [form, setForm] = useState({
-        ci: "",
-        password: "",
-    });
+  const [form, setForm] = useState({
+    ci: "",
+    password: "",
+  });
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        e.preventDefault();
-        setForm({ ...form, [e.target.name]: e.target.value });
-    };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-        const loginCorrecto = true;
+    const loginCorrecto = true;
 
-        if (!loginCorrecto) {
-            alert("Datos inválidos");
-            return;
-        }
+    if (!loginCorrecto) {
+      alert("Datos inválidos");
+      return;
+    }
 
-        if (tipo === "cliente") router.push("/cliente/inicio");
-        else if (tipo === "empleado") router.push("/empleado/inicio");
-        else router.push("/");
-    };
+    if (tipo === "cliente") router.push("/cliente/inicio");
+    else if (tipo === "empleado") router.push("/empleado/inicio");
+    else router.push("/");
+  };
 
-    return { handleChange, handleSubmit };
+  return {
+    handleChange,
+    handleSubmit,
+    form,
+  };
 }
